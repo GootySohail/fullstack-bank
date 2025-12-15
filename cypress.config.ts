970920eb03d2aa08bc5f437bf82b105230ab0c21
GootySohail <109505341+GootySohail@users.noreply.github.com>
2025-12-15 22:32:30 +0530

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,7 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
-    baseUrl: 'http://localhost:3000'
-  },
-
-  viewportHeight: 720,
-  viewportWidth: 1270,
-
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
-  },
+    baseUrl: "http://localhost:3000"
+  }
 });
